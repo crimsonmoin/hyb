dba7d91ca01cba94d666e3a2fdfc70c3b1acd3ae
crimsonmoin <crimsonmoin@yahoo.com>
2016-01-18 17:16:20 +0530

    idle timer for inactivity

---
+++ js/idleTimer.js
@@ -0,0 +1,16 @@
+idleTimer = null;
+idleState = false;
+idleWait = 15000;
+(function ($) {
+    $(document).ready(function () {   
+        $('*').bind('mousemove keydown scroll', function () {
+            clearTimeout(idleTimer);            
+            idleState = false;
+            idleTimer = setTimeout(function () { 
+			if(window.location.href!="#summarypage")
+			{window.location.href="#mainpage";}
+                idleState = true; }, idleWait);
+        });
+        $("body").trigger("mousemove");
+    });
+}) (jQuery)
