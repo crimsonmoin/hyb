60cae7fa3537b6cbfa9c0c664de881d2e7c1647b
crimsonmoin <crimsonmoin@yahoo.com>
2016-01-19 11:55:07 +0530

    main page idler fix

--- js/idleTimer.js
+++ js/idleTimer.js
@@ -10,7 +10,7 @@ idleWait = 20000;
 			var str=window.location.href;
 			var res = str.split("#");
 			//alert(res[1]);
-			if(res[1]!="summarypage")
+			if(res[1]!="summarypage"&&res[1]!="mainpage")
 			{window.location.href="#mainpage";}
                 idleState = true; }, idleWait);
         });
