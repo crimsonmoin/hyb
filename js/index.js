44b6fb6cad831c1bfb55bb4769b80acc27fc7086
crimsonmoin <crimsonmoin@yahoo.com>
2016-01-20 19:37:33 +0530

    eliminating insomnia mode

--- js/index.js
+++ js/index.js
@@ -33,7 +33,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        window.plugins.insomnia.keepAwake();
 		document.addEventListener("backbutton", function (e) {
 		e.preventDefault();
 		}, false);
